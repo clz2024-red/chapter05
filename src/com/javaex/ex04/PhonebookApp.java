b2cfea3c2a14ba2562f45b889ebe2cf8b58afb7f
clz2024.red <clz2024.red@gmail.com>
2024-01-16 13:08:48 +0900

    IOStream

---
+++ src/com/javaex/ex04/PhonebookApp.java
@@ -0,0 +1,13 @@
+package com.javaex.ex04;
+
+public class PhonebookApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//쓰기 빨대 쓰면 파일 다 지워짐
+		
+		//파일에서 읽은 내용을 리스트로 관리
+		//(이름, 핸드폰, 회사 ) --> Person
+	}
+
+}
